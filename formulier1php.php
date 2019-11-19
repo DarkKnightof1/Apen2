c41f7fbc275ce2dff3b43d8fb1d63e535e11bccc
DarkKnightof1 <mischa@boskat.eu>
2019-11-19 08:34:22 +0100

    1e versie

--- formulier1php.php
+++ formulier1php.php
@@ -5,3 +5,16 @@
  * Date: 15-11-2019
  * Time: 10:14
  */
+
+$dbh = new PDO('mysql:host=localhost;dbname=boodschappen2;port=8889', 'root', 'root');
+$boodschappen = $dbh->query('SELECT * from product');
+foreach ($boodschappen as $boodschap) {
+    echo "<li>".$boodschap{'omschrijving'}."</li>";
+}
+
+if(isset($_GET{'idboodschap'})) {
+    $idproduct = $_GET{'idproduct'};
+    $omschrijving = $_GET{'omschrijving'};
+    $sql = "insert into boodschappen2 (idproduct, omschrijving) values ($idproduct, '$omschrijving')";
+    $dbh->query($sql);
+}
