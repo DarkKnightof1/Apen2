10f2a9f0153497ada16f790901df8886eb88389e
DarkKnightof1 <mischa@boskat.eu>
2019-11-19 08:46:24 +0100

    database online

--- formulier1php.php
+++ formulier1php.php
@@ -6,15 +6,31 @@
  * Time: 10:14
  */
 
-$dbh = new PDO('mysql:host=localhost;dbname=boodschappen2;port=8889', 'root', 'root');
+$dbh = new PDO('mysql:host=localhost;dbname=apen;port=8889', 'root', 'root');
 $boodschappen = $dbh->query('SELECT * from product');
-foreach ($boodschappen as $boodschap) {
-    echo "<li>".$boodschap{'omschrijving'}."</li>";
-}
 
-if(isset($_GET{'idboodschap'})) {
-    $idproduct = $_GET{'idproduct'};
-    $omschrijving = $_GET{'omschrijving'};
-    $sql = "insert into boodschappen2 (idproduct, omschrijving) values ($idproduct, '$omschrijving')";
-    $dbh->query($sql);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//foreach ($boodschappen as $boodschap) {
+   // echo "<li>".$boodschap{'omschrijving'}."</li>";
+//}
+
+//if(isset($_GET{'idboodschap'})) {
+   // $idproduct = $_GET{'idproduct'};
+  //  $omschrijving = $_GET{'omschrijving'};
+  //  $sql = "insert into boodschappen2 (idproduct, omschrijving) values ($idproduct, '$omschrijving')";
+  //  $dbh->query($sql);
+//}
